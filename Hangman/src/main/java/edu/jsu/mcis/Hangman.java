90e796bc462452754f60eb7c3c17ea436eb40854
Joshua <joshua.a.hester@gmail.com>
2014-09-16 14:32:14 -0500

    -_-

--- Hangman/src/main/java/edu/jsu/mcis/Hangman.java
+++ Hangman/src/main/java/edu/jsu/mcis/Hangman.java
@@ -6,7 +6,9 @@ public class Hangman {
     public enum Result { WIN, LOSE, NONE };
     private String word;
     private List<Character> usedLetters;
-    
+ //   private String allLetters = "abcdefghijklmnopqrstuvwxyz";
+	private int guessNumber=0;
+	
     public Hangman() {
         word = "";
         usedLetters = new ArrayList<Character>();
@@ -16,18 +18,35 @@ public class Hangman {
         this.word = word;
     }
     
-    public boolean available(char c) {
-        return true;
+    public boolean available(char c) { 
+		if(this.usedLetters.isEmpty()){
+			this.usedLetters.add(c);
+			return true;
+		}
+		else if(this.usedLetters.contains(c))
+			return false;
+		else{
+			this.usedLetters.add(c);
+			return true;
+		}
     }
     
     public int guess(char c) {
-        return 2;
+		this.guessNumber++;
+		int occur =0;
+		for(int i;i<word.length();i++){
+			if(c==word.charAt(i))
+			occur++;
+		}
+		
+        return occur;
     }
     
     public Result getResult() {
         return Result.NONE;
     }
     
+	
 }
 
 
--- Hangman/src/test/java/edu/jsu/mcis/HangmanTest.java
+++ Hangman/src/test/java/edu/jsu/mcis/HangmanTest.java
@@ -62,5 +62,5 @@ public class HangmanTest {
     public void testGuessingEveryLetterBeforeSixMissesCausesAWin() {
         assertTrue(false);
     }
-    
+  
 }
